f70b11e0f026b9019f802b93348d4769696f12f1
ArkhayDS <raphael.ds@outlook.fr>
2023-02-13 15:32:56 +0100

    adding Icons, Font. Creating Atoms component

---
+++ src/Components/Atoms/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Icon({name, color}) {
+    // TODO - 1/ logic Name vs Path. 2/ add properties
+    // color - size(S/M/L) - weight - alignement
+
+    return (
+        <i></i>
+    );
+}
+
+export default Icon;
---
+++ src/Components/Atoms/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Text({htmltag}) {
+    // TODO - add properties
+    // font - color - fontsize - fontweight - lineheight - HTMLTag - alignement
+
+    return (
+        <htmltag></htmltag>
+    );
+}
+
+export default Text;
